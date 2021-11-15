ba197b2b6cd021319167b466ee17e76d61125fdb
Himanshi <kanojiahimanshi1221@gmail.com>
2021-11-15 19:42:35 +0530

    added logos for the societies

--- src/method-utilities/soc_content.js
+++ src/method-utilities/soc_content.js
@@ -1,5 +1,13 @@
+import venatus from '../assets/venatus.png'
+import junoon from '../assets/junoon.png'
+import cresendo from '../assets/cresendo.png'
+import alliance from '../assets/alliance.png'
+import debsoc from '../assets/debsoc.png'
+import tatsam from '../assets/tatsam.png'
+import dsc from '../assets/dsc.png'
 export const societies = [
     {
+        img:venatus,
         name: "Venatus",
         tagline: "The Gaming Society of NSUT",
         desc: "Venatus is known to provide high-end gear for event management and create major events attracting crowd from outside Delhi as well. Our gamers have been very notable in the gaming competitions held throughout Delhi, retaining their lead in every major tournament. Tournaments and Events: We keep two annual events every year on our college campus, namely: AIMBOT for Esports and SEEK: for Outdoors.",
@@ -9,6 +17,7 @@ export const societies = [
         fb: "https://www.facebook.com/VenatusNSIT/",
     },
     {
+        img:junoon,
         name: "Junoon",
         tagline: "The Photography Club of NSUT",
         desc: "The Photography Club of NSUT embodies the photography enthusiasts of the college. It is dedicated to the enhancement and development of skills in photography, graphic designing, video editing, etc. We organize numerous photo-walks, workshops, and events throughout the year and are also responsible for capturing all the events at our university.",
@@ -18,6 +27,7 @@ export const societies = [
         fb: "https://www.facebook.com/junoon.nsut/",
     },
     {
+        img:cresendo,
         name: "Cresendo",
         tagline: "The music society of NSUT",
         desc: "Based within the confines of a hundred acres of campus in West Delhi, but the idea penetrating not only just the walls of the campus but of the states of our country, Crescendo is the music society of NSUT. We're a group of to-be engineers, but we produce sounds of change, character and substance. Our society is a clear, pure and unrestrained expression of numerous forms of music, be it Indian classical, western classical, rock, electronic, rap, instrumental and fusion music. Our ideas and motives are simple: we see a world, starting from our immediate vicinity, where music is allowed to flow without any restriction, with utmost sympathy and with a strong objective of change, be it in oneself or a cause they feel attached to. For that very purpose, Crescendo has been working for over half a decade now to establish itself in the renowned cultural circuits of Delhi, and subsequently, India. We have been an epicenter of this visceral art, adding to our group of talented and imaginative musicians every year. We see music as an art that never discriminates and always welcomes ingenuity.",
@@ -27,6 +37,7 @@ export const societies = [
         fb: "https://www.facebook.com/Crescendo.NSUT/",
     },
     {
+        img:alliance,
         name: "The Alliance",
         tagline: "NSUT's Newspaper",
         desc: "The Alliance is the official newspaper society of NSUT. We provide a platform to you where creative content, critical thinking and imagination thrive. Apart from being one of the oldest and most recognised societies of NSUT, what makes this society so revered by its members are the brilliant, helpful and adroit people you’ll meet here and eventually get to call friends. The Alliance is an assortment of creative minds and original thinkers, which allows you to not only explore different horizons but also makes you a part of an affectionate family, forming bonds which last for a lifetime.",
@@ -36,6 +47,7 @@ export const societies = [
         fb: "https://m.facebook.com/nsitalliance",
     },
     {
+        img:debsoc,
         name: "Debsoc",
         tagline: "The Debating Society of NSUT",
         desc: "The Debating Society of NSUT popularly known as “DebSoc” is a proponent of deliberation and discourse and serves as a platform for young minds to shed their inhibitions and learn the art of expression. Throughout the years it has built an exemplary reputation in the debating circuits, with achievements at the national as well as the international levels including the Harvard MUN and Singapore MUN. The society functions through its different departments, namely the Parliamentary Debating, Model UN, General Debating, Literary department, along with a Tech and Design department.",
@@ -45,6 +57,7 @@ export const societies = [
         fb: "https://www.facebook.com/DebSocNSUT/",
     },
     {
+        img:tatsam,
         name: "Tatsam",
         tagline: "The Hindi Society of NSUT",
         desc: "तत्सम नेताजी सुभाष प्रौद्योगिकी संस्थान के छात्रों में हिंदी वाचन व लेखन को प्रोत्साहित करने के लिए प्रतिबद्ध है | “तत्सम” संस्थान के सभी हिंदी में रूचि रखने वाले छात्रों का प्रतिनिधित्व करती है | हमारा ध्येय मूलतः संस्थान में हिंदी के मौखिक एवं लिखित स्तर को विकसित करना और हिंदी के प्रति छात्रों में रुझान पैदा करना है |",
@@ -54,6 +67,7 @@ export const societies = [
         fb: "https://www.facebook.com/tatsam.nsut/",
     },
      {
+        img:dsc,
         name: "DSC",
         tagline: "Developer Student Clubs",
         desc: "Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.",
