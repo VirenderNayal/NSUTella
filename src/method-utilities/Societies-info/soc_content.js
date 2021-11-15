72b94bf97e162d4c315eab85ee52cd3e60c055f8
Himanshi <76556340+Himanshi1207@users.noreply.github.com>
2021-11-15 14:14:18 +0530

    Update soc_content.js

--- src/method-utilities/Societies-info/soc_content.js
+++ src/method-utilities/Societies-info/soc_content.js
@@ -26,65 +26,45 @@ export const societies = [
         ig: "https://instagram.com/crescendonsut?utm_medium=copy_link",
         fb: "https://www.facebook.com/Crescendo.NSUT/",
     },
+    {
+        name: "The Alliance",
+        tagline: "",
+        desc: "The Alliance is the official newspaper society of NSUT. We provide a platform to you where creative content, critical thinking and imagination thrive. Apart from being one of the oldest and most recognised societies of NSUT, what makes this society so revered by its members are the brilliant, helpful and adroit people you’ll meet here and eventually get to call friends. The Alliance is an assortment of creative minds and original thinkers, which allows you to not only explore different horizons but also makes you a part of an affectionate family, forming bonds which last for a lifetime.",
+        web: "https://alliancensut.com/",
+        linkedin: "https://www.linkedin.com/company/the-alliance-nsit-newspaper",
+        ig: "https://instagram.com/alliance_nsut?utm_medium=copy_link",
+        fb: "https://m.facebook.com/nsitalliance",
+    },
+    {
+        name: "Debsoc",
+        tagline: "The Debating Society of NSUT",
+        desc: "The Debating Society of NSUT popularly known as “DebSoc” is a proponent of deliberation and discourse and serves as a platform for young minds to shed their inhibitions and learn the art of expression. Throughout the years it has built an exemplary reputation in the debating circuits, with achievements at the national as well as the international levels including the Harvard MUN and Singapore MUN. The society functions through its different departments, namely the Parliamentary Debating, Model UN, General Debating, Literary department, along with a Tech and Design department.",
+        web: "",
+        linkedin: "https://www.linkedin.com/company/debating-society-of-nsit/",
+        ig: "https://instagram.com/debsocnsut?utm_medium=copy_link",
+        fb: "https://www.facebook.com/DebSocNSUT/",
+    },
+    {
+        name: "Tatsam",
+        tagline: "",
+        desc: "तत्सम नेताजी सुभाष प्रौद्योगिकी संस्थान के छात्रों में हिंदी वाचन व लेखन को प्रोत्साहित करने के लिए प्रतिबद्ध है | “तत्सम” संस्थान के सभी हिंदी में रूचि रखने वाले छात्रों का प्रतिनिधित्व करती है | हमारा ध्येय मूलतः संस्थान में हिंदी के मौखिक एवं लिखित स्तर को विकसित करना और हिंदी के प्रति छात्रों में रुझान पैदा करना है |",
+        web: "",
+        linkedin: "https://www.linkedin.com/company/tatsam",
+        ig: "https://instagram.com/tatsam.nsut?utm_medium=copy_link",
+        fb: "https://www.facebook.com/tatsam.nsut/",
+    },
+     {
+        name: "DSC",
+        tagline: "Developer Student Clubs",
+        desc: "Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.",
+        web: "https://gdsc.community.dev/netaji-subhas-university-of-technology-delhi/",
+        linkedin: "https://www.linkedin.com/company/dscnsut",
+        ig: "https://instagram.com/dsc_nsut?utm_medium=copy_link",
+        fb: "https://www.facebook.com/dscnsit17/",
+    },
 ]
 
 /*
-3. Cresendo
-
-Based within the confines of a hundred acres of campus in West Delhi, but the idea penetrating not only just the walls of the campus but of the states of our country, Crescendo is the music society of NSUT. We're a group of to-be engineers, but we produce sounds of change, character and substance. Our society is a clear, pure and unrestrained expression of numerous forms of music, be it Indian classical, western classical, rock, electronic, rap, instrumental and fusion music. Our ideas and motives are simple: we see a world, starting from our immediate vicinity, where music is allowed to flow without any restriction, with utmost sympathy and with a strong objective of change, be it in oneself or a cause they feel attached to. For that very purpose, Crescendo has been working for over half a decade now to establish itself in the renowned cultural circuits of Delhi, and subsequently, India. We have been an epicenter of this visceral art, adding to our group of talented and imaginative musicians every year. We see music as an art that never discriminates and always welcomes ingenuity.
-
-https://instagram.com/crescendonsut?utm_medium=copy_link
-
-https://www.linkedin.com/company/crescendo-nsut/
-
-https://www.facebook.com/Crescendo.NSUT/
-
-
-4. The Alliance
-
-The Alliance is the official newspaper society of NSUT. We provide a platform to you where creative content, critical thinking and imagination thrive. Apart from being one of the oldest and most recognised societies of NSUT, what makes this society so revered by its members are the brilliant, helpful and adroit people you’ll meet here and eventually get to call friends. The Alliance is an assortment of creative minds and original thinkers, which allows you to not only explore different horizons but also makes you a part of an affectionate family, forming bonds which last for a lifetime.
-
-https://alliancensut.com/
-
-https://instagram.com/alliance_nsut?utm_medium=copy_link
-
-https://www.linkedin.com/company/the-alliance-nsit-newspaper
-
-https://m.facebook.com/nsitalliance
-
-
-
-5. Debsoc- The Debating Society of NSUT
-
-The Debating Society of NSUT popularly known as “DebSoc” is a proponent of deliberation and discourse and serves as a platform for young minds to shed their inhibitions and learn the art of expression. Throughout the years it has built an exemplary reputation in the debating circuits, with achievements at the national as well as the international levels including the Harvard MUN and Singapore MUN. The society functions through its different departments, namely the Parliamentary Debating, Model UN, General Debating, Literary department, along with a Tech and Design department.
-
-https://instagram.com/debsocnsut?utm_medium=copy_link
-
-https://www.linkedin.com/company/debating-society-of-nsit/
-
-https://www.facebook.com/DebSocNSUT/
-
-
-6. Tatsam
-
-"तत्सम" नेताजी सुभाष प्रौद्योगिकी संस्थान के छात्रों में हिंदी वाचन व लेखन को प्रोत्साहित करने के लिए प्रतिबद्ध है | “तत्सम” संस्थान के सभी हिंदी में रूचि रखने वाले छात्रों का प्रतिनिधित्व करती है | हमारा ध्येय मूलतः संस्थान में हिंदी के मौखिक एवं लिखित स्तर को विकसित करना और हिंदी के प्रति छात्रों में रुझान पैदा करना है |
-
-https://instagram.com/tatsam.nsut?utm_medium=copy_link
-
-https://www.linkedin.com/company/tatsam
-
-https://www.facebook.com/tatsam.nsut/
-
-7.  DSC – Developer Student Clubs
-Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.
-
-https://gdsc.community.dev/netaji-subhas-university-of-technology-delhi/
-
-https://instagram.com/dsc_nsut?utm_medium=copy_link
-
-https://www.linkedin.com/company/dscnsut
-
-https://www.facebook.com/dscnsit17/
 Subhasha
 
 https://instagram.com/subhasha.nsut?utm_medium=copy_link
