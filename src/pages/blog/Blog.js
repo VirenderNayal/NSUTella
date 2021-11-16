e268f66cec6333667aadd2591202e9aaf0916c95
Wizbeans <dixitmadhavi@yahoo.com>
2021-11-16 11:26:27 +0530

    only content added on blogs

--- src/pages/blog/Blog.js
+++ src/pages/blog/Blog.js
@@ -26,8 +26,8 @@ export default function Blog() {
                             <div className="d-flex justify-content-center align-items-start">
                                 <p>01</p>
                                 <div class="card-body px-3 py-0">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supportiral lead-in to additional content.</p>
+                                    <h5 class="card-title">Metaverse</h5>
+                                    <p class="card-text">combination of multiple elements of technology, including virtual reality, augmented reality and video where users "live" within a digital universe.</p>
                                     <p><span>Nov 14</span> - <span>5 min</span></p>
                                 </div>
                             </div>
@@ -38,8 +38,9 @@ export default function Blog() {
                             <div className="d-flex justify-content-center align-items-start">
                                 <p>02</p>
                                 <div class="card-body px-3 py-0">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supportiral lead-in to additional content.</p>
+                                    <h5 class="card-title">E-Sports</h5>
+                                    <p class="card-text">esports are direct competitions between human players, using suitable video and computer games under fixed rules.
+</p>
                                     <p><span>Nov 14</span> - <span>5 min</span></p>
                                 </div>
                             </div>
@@ -50,8 +51,8 @@ export default function Blog() {
                             <div className="d-flex justify-content-center align-items-start">
                                 <p>03</p>
                                 <div class="card-body px-3 py-0">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supportiral lead-in to additional content.</p>
+                                    <h5 class="card-title">Extended Reality (XR)</h5>
+                                    <p class="card-text">might be the Future of Education, Gaming, Entertainment, E-commerce. The scope of application of XR is so much because the X in XR stands for X variable..</p>
                                     <p><span>Nov 14</span> - <span>5 min</span></p>
                                 </div>
                             </div>
@@ -66,9 +67,9 @@ export default function Blog() {
                         <div class="card">
                             <div className="d-flex">
                                 <div class="card-body">
-                                    <h6 class="card-title">Card title</h6>
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content</p>
+                                    <h6 class="card-title">BLOCK CHAIN</h6>
+                                    <h5 class="card-title">Block Chain Technology- Beyond Cryptocurrencies</h5>
+                                    <p class="card-text">increasingly being used by various industries to address issues ranging from day to day operations</p>
                                     <p><span>Oct 27</span> - <span>15 min</span></p>
                                 </div>
                                 <img src="https://picsum.photos/200/150" alt="pic" />
@@ -79,9 +80,9 @@ export default function Blog() {
                         <div class="card">
                             <div className="d-flex">
                                 <div class="card-body">
-                                    <h6 class="card-title">Card title</h6>
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content</p>
+                                    <h6 class="card-title">5G</h6>
+                                    <h5 class="card-title">5th generation of broadband technology</h5>
+                                    <p class="card-text">5G was rolled out in 2019 by cellular phone companies and the top smart phones and IoT brands have started manufacturing the 5G enabled devices in large scale from 2020.</p>
                                     <p><span>Oct 27</span> - <span>15 min</span></p>
                                 </div>
                                 <img src="https://picsum.photos/200/150" alt="pic" />
@@ -92,9 +93,22 @@ export default function Blog() {
                         <div class="card">
                             <div className="d-flex">
                                 <div class="card-body">
-                                    <h6 class="card-title">Card title</h6>
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content</p>
+                                    <h6 class="card-title">AUTOMATION</h6>
+                                    <h5 class="card-title">Self Driven Cars and Drones </h5>
+                                    <p class="card-text">Self Driven Cars and Drones enabled by automation logics and algorithms is one of the emerging technologies in 2021. This industry is in its prime stage to boom in 2021 and this entire decade in general.</p>
+                                    <p><span>Oct 27</span> - <span>15 min</span></p>
+                                </div>
+                                <img src="https://picsum.photos/200/150" alt="pic" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div className="d-flex">
+                                <div class="card-body">
+                                    <h6 class="card-title">IOT</h6>
+                                    <h5 class="card-title">Bluetooth enabled Beacon Technology </h5>
+                                    <p class="card-text">Broad range of IoT applications powered by internet as well as Bluetooth Connectivity is opening up new opportunities across various sectors</p>
                                     <p><span>Oct 27</span> - <span>15 min</span></p>
                                 </div>
                                 <img src="https://picsum.photos/200/150" alt="pic" />
