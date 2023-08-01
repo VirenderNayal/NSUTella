bd9da47c8f53a35f555bebe7f9d6326f1ceac78d
Virender Nayal <virendernayal3003@gmail.com>
2023-08-01 22:29:13 +0530

    Bug fixes and firebase deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyDgNwTA7yw1nLJWqITgrZXijU-8JX7Ma4E",
